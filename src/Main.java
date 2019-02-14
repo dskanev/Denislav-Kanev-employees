e46fa35a44aec97a17322d3076762048ceb65c87
Denislav Kanev <denk@itu.dk>
2019-02-14 13:56:03 +0200

    calculateCollabTime(Entry e1, Entry e2) successfully calculates the overlapping days of 2 Entry objects

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.sql.Date;
+
 
 
 
@@ -15,7 +17,9 @@ public class Main {
 
     public static void main (String[] args){
         ArrayList<Entry> entries = inputReader("input.txt");
-
+        Entry entry1 = entries.get(0);
+        Entry entry2 = entries.get(1);
+        System.out.println(calculateCollabTime(entry1,entry2));
         }
 
 
@@ -57,7 +61,6 @@ i.	Repeat for all employees
                 String[] lineOfInput = line.split(", ");
                 if (lineOfInput[3].equalsIgnoreCase("null")){
                     lineOfInput[3] = LocalDate.now().toString();
-                    System.out.println(lineOfInput[3]);
                 }
                 input.add(new Entry(Integer.parseInt(lineOfInput[0]), Integer.parseInt(lineOfInput[1]), Date.valueOf(lineOfInput[2]), Date.valueOf(lineOfInput[3])));
             }
@@ -70,6 +73,27 @@ i.	Repeat for all employees
         }
        return input;
     }
-    
+    private static int calculateCollabTime(Entry entry1, Entry entry2){
+        if (entry1.getDateFrom().before(entry2.getDateFrom())){
+            if (entry1.getDateTo().after(entry2.getDateTo())){
+                return getTimeBetweenSQLDates(entry2.getDateFrom(),entry2.getDateTo());
+            } else {
+                return getTimeBetweenSQLDates(entry2.getDateFrom(),entry1.getDateTo());
+            }
 
+        }
+        else if (entry1.getDateFrom().after(entry2.getDateFrom())){
+            if (entry1.getDateTo().before(entry2.getDateTo())){
+                return getTimeBetweenSQLDates(entry1.getDateFrom(),entry1.getDateTo());
+            } else {
+                return getTimeBetweenSQLDates(entry1.getDateFrom(),entry2.getDateTo());
+            }
+        }
+        return-1;
+    }
+    private static int getTimeBetweenSQLDates(Date date1, Date date2){
+        long temp = ChronoUnit.DAYS.between(LocalDate.parse(date1.toString()),LocalDate.parse(date2.toString()));
+        int result = (int)temp;
+        return result;
+    }
 }
